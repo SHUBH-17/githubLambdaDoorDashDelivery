747a02572d0803328c43152338e20e736d78fa40
my name <my email>
2024-03-15 13:48:23 +0530

    Updated Lambda Function

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import boto3
 import json
-from datetime import datetime
 
 s3 = boto3.client('s3')
 sns_client = boto3.client('sns')
@@ -10,28 +9,21 @@ sns_arn = 'arn:aws:sns:ap-south-1:112207118291:doordash-data-processing-notifica
 def lambda_handler(event, context):
     try:
 
-        print(event)
+        print("Event: ",event)
 	
 	    # Define the source bucket name and file name
         source_bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
         source_file_name = event["Records"][0]["s3"]["object"]["key"]
 
-        print(source_bucket_name)
-        print(source_file_name)
+        print("Source Bucket: ",source_bucket_name)
+        print("Source File: ",source_file_name)
 		
 		# Download the JSON file from the source bucket
         response = s3.get_object(Bucket=source_bucket_name, Key=source_file_name)
         json_data = response['Body'].read().decode('utf-8')
         
-        # Parse the JSON data
-        data = json.loads(json_data)
-
-        # Ensure consistent date format
-        for item in data:
-          item['date'] = datetime.strptime(item['date'], '%Y-%m-%d').date()
-
-        # Convert the parsed data to a Pandas DataFrame
-        df = pd.DataFrame(data)
+        # Convert the JSON data to a Pandas DataFrame
+        df = pd.read_json(json_data)
         
         # Filter the records based on certain criteria
         filtered_df = df[df['status'] == 'delivered']
@@ -45,12 +37,12 @@ def lambda_handler(event, context):
         destination_bucket_name = 'doordash-target-zn-sm'
         destination_file_name = 'delivered_records.json'
 
-        print(destination_bucket_name)
-        print(destination_file_name)
+        print("Destination Bucket: ",destination_bucket_name)
+        print("Destination File: ",destination_file_name)
         
         # Upload the JSON string to the destination bucket
         s3.put_object(Bucket=destination_bucket_name, Key=destination_file_name, Body=json_string)
-        message = "Filtered records have been written to {}".format("s3://"+destination_bucket_name+"/"+destination_file_name)
+        message = "Filtered records have been written to {}/{}".format("s3://"+destination_bucket_name+"/"+destination_file_name)
 		
         respone = sns_client.publish(Subject="SUCCESS - Daily Data Processing",TargetArn=sns_arn, Message=message, MessageStructure='text')
 		
